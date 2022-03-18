eba73d67df5812ce945190b188f862ce063ea008
Maria Luisa  Araujo Lima <rm79912@estudante.fieb.edu.br>
2022-03-18 10:53:06 -0300

    Primeiro Commit

---
+++ src/ResultadoAluno.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class ResultadoAluno {
+
+	public static void main(String[] args) {
+		
+		double media = 6.0;
+		
+		JOptionPane.showMessageDialog(null, "Aprovado!");
+		} else {
+			JOptionPane.showMessageDialog(null, "Reprovado!");
+			}
+				
+	    
+	}
+
+}
+
